75defb3172dc659417072026148e8390d7db8ac2
Adam Zekiec <adam.zekiec@gmail.com>
2026-01-23 14:38:53 +0100

    Ensured foreign key constraints

--- Raspberry/db_init.py
+++ Raspberry/db_init.py
@@ -18,6 +18,7 @@ def main():
 
     conn = sqlite3.connect(DB_PATH)
     conn.execute("PRAGMA journal_mode=WAL;")
+    conn.execute("PRAGMA foreign_keys = ON")
     c = conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS devices
                  (mac TEXT PRIMARY KEY,
